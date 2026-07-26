83d9a2e135711844ae6d11e9ee55c77ce26e4fbb
Luukas Lehtinen <luukas.k.lehtinen@gmail.com>
2026-07-26 05:05:25 +0300

    update fetch url

--- src/hooks/useEntries.ts
+++ src/hooks/useEntries.ts
@@ -12,7 +12,7 @@ const useEntries = () => {
 	useEffect(() => {
 		setIsLoading(true)
 
-		fetch('/sample-data/data.json')
+		fetch('/test-ictadmission/sample-data/data.json')
 			.then(async response => {
 				const payload = await response.json()
 				const parsed = z.array(entrySchema).safeParse(payload)
